15b902d0ab0b9193ae7510f1322f2aaa0dd04be1
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-10-10 14:00:45 +0700

    create model Task

--- mysite/todo_app/models.py
+++ mysite/todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Ta sẽ sử dụng ORM trong django để ánh xạ những class được tạo ra trong models.py thành 1 bảng trong database.
+"""
+
+class Task (models.Model):
+    title = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
