713e5f7666d21ff6393ad9e7a0f49652e1149232
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2024-10-10 10:50:34 +0700

    Part4

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
